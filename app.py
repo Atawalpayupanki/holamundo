7209f78b33a9b92a15a114123ad977570db7c251
PauT <pautall667@gmail.com>
2024-11-20 19:08:00 +0100

    commit3

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit  as st
 st.title("lechuga")
 st.subheader("Lechugota")
 st.write("**lecuguita**")
-
+st.image("https://p16-oec-ttp.tiktokcdn-us.com/tos-useast5-i-omjb5zjo8w-tx/2815ecc9bcac4e75af2f71a1d0646136~tplv-omjb5zjo8w-crop-webp:400:400.webp?from=1476391136")
